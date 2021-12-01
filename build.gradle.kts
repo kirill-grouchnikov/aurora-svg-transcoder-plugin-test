dfa9336bec5eefdd0cd8f9fcf7f467990a4274b8
Kirill Grouchnikov <kirillg@google.com>
2021-12-01 13:45:59 -0500

    First drop

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://plugins.gradle.org/m2/")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+    }
+
+    dependencies {
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.0.0")
+        classpath(kotlin("gradle-plugin", version = "1.5.31"))
+        classpath("org.pushing-pixels:aurora-tools-svg-transcoder-gradle-plugin:1.0.0-beta4")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+    }
+
+    configurations {
+        all {
+            exclude(group = "org.jetbrains.compose.material", module = "material")
+        }
+    }
+}
